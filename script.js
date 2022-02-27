de7ccc99eb550f5cff79a92e0c89562d6da08754
Michael Hoyle <mcalebhoyle@gmail.com>
2022-02-26 23:34:59 -0500

    worked on adding logic to the password generation. Set up the framework for the remaining logic.

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var possibleChoices = [];
+for (var i = 8; i <= 128; i++) {
+  possibleChoices.push(i);
+}
 
 function generatePassword() {
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  
+
+  if (!count) {
+    alert('Select a valid character amount');
+  }
+
+  if (count != possibleChoices) {
+    alert('Please select a character amount ranging from 8 to 128');
+  }
+
+  var lower = prompt('Would you like lowercase letters in your password?')
+
+  var upper = prompt('Would you like uppercase letters in your password?');
+
+  var num = prompt('Would you like numbers in your password?');
+
+  var spCharacters = prompt('Would you like special characters in your password?')
+
   var letters = "abcdefghijklmnopqrstuvwxyz";
+  var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "~!@#$%&?_=+/.,'()\]["
   var genPassword = "";
 
   for (var i = 0; i < count; i++) {
-    var random = Math.floor(Math.random() * letters.length);
-    genPassword += letters[random];
+    genPassword += special[Math.floor(Math.random() * special.length)] + letters[Math.floor(Math.random() * letters.length)] + upperLetters[Math.floor(Math.random() * upperLetters.length)] + numbers[Math.floor(Math.random() * numbers.length)];
   }
 
   console.log(genPassword);
