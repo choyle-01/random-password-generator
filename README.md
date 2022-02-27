c17a38d2256747af1bcc843bde9842ff93ba4f68
Michael Hoyle <mcalebhoyle@gmail.com>
2022-02-27 16:00:05 -0500

    Finished up the project. Fixed remaining issues with undefined and issues printing password. Added comments to show the process of the program. Wrote up the README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # random-password-generator
+
+## Project Description
+In this project we were tasked with building a random password generator. The password generator is supposed to generate a random password that meets certain criteria that is specified by the user. The password is supposed to prompt the user to see if they would like to include certain character types as well as ask how many characters they would like it to be. This was achieved through alerts confirms and prompts to make things function the way they should. The pasword needed to be between 8 and 128 characters long. By using an if statement and an alert we were able to limit the character amount to only be what was requested. The types of characters that could go into the prompt needed to be lowercase letters, uppercase letters, special characters, and nunbers. The user is able to choose which of these they would like in their password and when they do their selection is then added into the character pool and that type of character is added to the password. The password is then generated and witten on the page. This was a fun assignment to do and was challenging to put together and figure out how to get everything to work correctly. In the end it ended up making a lot more sense and I feel like I leanred a lot about javascript during this project. The password has worked every time I've run it and I haven't had any scenarios where a value wasn't put into the password when it was supposed to be. I also went through and made sure empty strings would be printed out so that there wasn't a scenario in which undefined was an option that was printed out as the password.
+
+## Project Link
+The project can be found [here](https://choyle-01.github.io/random-password-generator/)
+
+GitHub Repository can be found [here](https://github.com/choyle-01/random-password-generator)
+
+## Visuals
+![photo of finished website](./image/website.png)
--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// create a function titled generatePassword 
 function generatePassword() {
+  // prompts the user to enter a value of how many charaters they would like in their password 
   var count = Number(
     prompt(
       "Please enter the amount of characters you would like in your password."
     )
   );
-
+  // if the password is not between 8 and 128 characters it promps the user to enter a valid character amount 
   if (count < 8 || count > 128) {
     alert("Password must be between 8 and 128 characters.");
     return "";
   }
-
+  // creates variables for all of the possible inputs into the password as well as a genPassword variable that will output the password 
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var upperLetters = letters.toUpperCase();
   var numbers = "0123456789";
   var special = '~!@#$%^&*()_-+={}[]";:/?<>.,';
   var genPassword = "";
-  var charPool = '';
+  // creates a variable that allows users to choose what charcter types goes into their password 
+  var charPool = "";
 
-  if (confirm('Would you like lowercase letters in your password?')) {
+  // creates confirm statements that allows users to append character types to their passwords 
+  if (confirm("Would you like lowercase letters in your password?")) {
     charPool += letters;
   }
-
 
   if (confirm("Would you like uppercase letters in your password?")) {
     charPool += upperLetters;
@@ -35,24 +36,28 @@ function generatePassword() {
 
   if (confirm("Would you like special characters in your password?")) {
     charPool += special;
+    // creates an else statement in case the user decided to decline all character types
+  } else {
+    alert("Please select a valid character amount.");
+    return "";
   }
 
+  // creates a for loop that takes in the count number and increments through the character pool randomly with the selected character types
   for (var i = 0; i < count; i++) {
     var random = Math.floor(Math.random() * charPool.length);
     genPassword += charPool[random];
 
     console.log(genPassword);
-
-    return genPassword;
   }
+// returns the generated password
+  return genPassword;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-  }
+  passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
